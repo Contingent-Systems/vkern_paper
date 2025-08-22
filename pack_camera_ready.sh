dae2a3503ca810b735f9cbc93dae16f631cfc678
Colin S. Gordon <colin.s.gordon@gmail.com>
2025-08-21 20:47:14 -0400

    add script to package cameraready zip

---
+++ pack_camera_ready.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+zip cameraready.zip addr_translation.png *.cls *.sty paper.tex paper.bbl intro.tex background.tex syntaxsemantics.tex programlogic.tex examples_C.tex relwork.tex vmm.bib ACM-Reference-Format.bst
